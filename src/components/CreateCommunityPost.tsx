fe69ea22ba8d7c25bb11693d14de7f93e34606ad
Prabish-work <prabish.dangi@deerwalk.edu.np>
2024-03-27 00:17:19 +0545

    Create post bar on community page | to-do community feed post

--- src/components/CreateCommunityPost.tsx
+++ src/components/CreateCommunityPost.tsx
@@ -1,17 +1,22 @@
+"use client";
+
 import { Session } from "next-auth";
 import { usePathname, useRouter } from "next/navigation";
 import UserAvatar from "./UserAvatar";
+import { Input } from "./ui/Input";
+import { Button } from "./ui/Button";
+import { ImageIcon, Link2 } from "lucide-react";
 
 interface CreateCommunityPostProps {
   session: Session | null;
 }
 
 const CreateCommunityPost = ({ session }: CreateCommunityPostProps) => {
-  //   const router = useRouter();
-  //   const pathname = usePathname();
+  const router = useRouter();
+  const pathname = usePathname();
   return (
     <div className=" bg-white rounded-md shadow-lg">
-      <div className=" h-full px-6 py-4">
+      <div className=" h-full sm:flex sm:justify-between gap-6 px-6 py-4">
         <div className="relative">
           <UserAvatar
             user={{
@@ -22,6 +27,13 @@ const CreateCommunityPost = ({ session }: CreateCommunityPostProps) => {
 
           <span className="absolute outline outline-2 bottom-0 right-0 bg-green-500 outline-white h-3 w-3 rounded-full" />
         </div>
+        <Input className="rounded-xl" readOnly placeholder="Write something...." onClick={() => router.push(pathname + "/submit")} />
+        <Button variant="ghost" onClick={() => router.push(pathname + "/submit")}>
+          <ImageIcon className="text-zinc-600" />
+        </Button>
+        <Button variant="ghost" onClick={() => router.push(pathname + "/submit")}>
+          <Link2 className="text-zinc-600" />
+        </Button>
       </div>
     </div>
   );
