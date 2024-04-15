f2fa573e0d671dd8fe9e30e9ff0b625905308212
Prabish Personal <prabish.dangi@deerwalk.edu.np>
2024-04-15 09:44:03 +0545

    push to vercel

---
+++ src/components/post-vote/PostVoteServer.tsx
@@ -0,0 +1,44 @@
+import { getAuthSession } from "@/lib/auth";
+import { Post, User, Vote, VoteType } from "@prisma/client";
+import { notFound } from "next/navigation";
+import React from "react";
+import PostVoteClient from "./PostVoteClient";
+
+interface PostVoteServerProps {
+  postId: string;
+  initialVotesAmt: number;
+  initialVote?: VoteType | null;
+  getData?: () => Promise<(Post & { votes: Vote[]; author: User }) | null>;
+}
+
+const PostVoteServer = async ({ postId, initialVote, initialVotesAmt, getData }: PostVoteServerProps) => {
+  const session = await getAuthSession();
+
+  let _voteAmt: number = 0;
+  let _currentVote: VoteType | null | undefined = undefined;
+
+  if (getData) {
+    const post = await getData();
+    if (!post) {
+      return notFound();
+    }
+
+    _voteAmt = post.votes.reduce((acc, vote) => {
+      if (vote.type === "UP") {
+        return acc + 1;
+      }
+      if (vote.type === "DOWN") {
+        return acc - 1;
+      }
+    }, 0);
+
+    _currentVote = post.votes.find((vote) => vote.userId === session?.user.id)?.type;
+  } else {
+    _voteAmt = initialVotesAmt!;
+    _currentVote = initialVote;
+  }
+
+  return <PostVoteClient postId={postId} initialVotesAmt={_voteAmt} initialVote={_currentVote} />;
+};
+
+export default PostVoteServer;
